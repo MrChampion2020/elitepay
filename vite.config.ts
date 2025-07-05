7f0e1d3cb163b7769335ca6785bdf74030ca9258
mrchampion2020 <kingedendgreat2017@gmail.com>
2025-07-05 08:45:57 +0100

    merge remote changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 });
